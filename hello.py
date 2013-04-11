10f3e277cd643a5d51bcfc720d23b1f7bd73aa15
Akshar Raaj <akshar@agiliq.com>
2013-04-11 16:03:07 +0530

    Add variable part to url

--- hello.py
+++ hello.py
@@ -10,6 +10,10 @@ def index():
 def hello_world():
     return 'Hello World!'
 
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return "Profile for %s" % (username,)
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0')
