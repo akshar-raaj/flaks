61bb4fea2a6a10f3dbebcd9482a0ca4885893645
Akshar Raaj <akshar@agiliq.com>
2013-04-11 16:50:01 +0530

    Add static directory, use url_for

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
     return 'Index page'
 
-@app.route('/hello')
+@app.route('/hello/')
 def hello_world():
     return 'Hello World!'
 
@@ -18,6 +18,11 @@ def show_user_profile(username):
 def show_post(post_id):
     assert isinstance(post_id, int)
     return "Post %d" % (post_id,)
+
+@app.route('/show_static/')
+def show_static_url():
+    url = url_for('static', filename='style.css')
+    return url
 
 if __name__ == '__main__':
     app.debug = True
